622bead2fd9770c38d44c6434f027cc51dc84826
Omisakin Joshua <officialjoshua9@gmail.com>
2024-02-11 18:44:48 +0100

    "updated the app"

--- app/src/main/java/com/example/monsterphonics/GameMainMenu.java
+++ app/src/main/java/com/example/monsterphonics/GameMainMenu.java
@@ -3,15 +3,29 @@ package com.example.monsterphonics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class GameMainMenu extends AppCompatActivity {
+
+    private Button btnSound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_main_menu);
+
+        btnSound = findViewById(R.id.btnSound);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this,R.raw.audio);
+
+        btnSound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v) {
+                mediaPlayer.start();
+            }
+        });
     }
 
     public void openGameLogic(View view) {
